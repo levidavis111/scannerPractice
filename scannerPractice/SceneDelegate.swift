3e48abc68a6a3981048ed6227cffdc4a3724e5dd
levi davis <levidavis@pursuit.org>
2020-05-12 13:28:19 -0400

    Removes storyboard and sets scene and app delegates

--- scannerPractice/SceneDelegate.swift
+++ scannerPractice/SceneDelegate.swift
@@ -17,9 +17,14 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let scene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.windowScene = scene
+        let mainVC = ViewController()
+        let navVC = UINavigationController(rootViewController: mainVC)
+        window?.makeKeyAndVisible()
     }
-
+    
     func sceneDidDisconnect(_ scene: UIScene) {
         // Called as the scene is being released by the system.
         // This occurs shortly after the scene enters the background, or when its session is discarded.
